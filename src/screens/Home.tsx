376cee0c884db18b376911fb4c3e8ad9b6777fd2
Faizanansari222 <faizanrazaansari07@gmail.com>
2024-10-30 00:15:13 +0500

    seccond commit

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,9 @@
 import {
   View,
+  Text,
   ScrollView,
+  FlatList,
+  FlatListComponent,
 } from 'react-native';
 import React from 'react';
 import Navbar from '../component/Navbar';
@@ -10,13 +13,13 @@ const Home = () => {
   return (
     <View>
       <Navbar />
-      <View style={{padding: 5,  justifyContent: 'center', }}>
+      <View style={{padding: 10, justifyContent: 'center', }}>
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={true}>
-          <Header heading="Graphic Desiging Course" durations="2 months" imgUrl={require('../assets/artistic-tools1.png')}/>
-          <Header heading="Graphic Desiging Course" durations="2 months" imgUrl={require('../assets/artistic-tools1.png')}/>
-          <Header heading="Graphic Desiging Course" durations="2 months" imgUrl={require('../assets/artistic-tools1.png')}/>
+          <Header heading="Graphic Desiging Course" durations="2 months" imgUrl={require('../assets/')}/>
+          <Header heading="Graphic Desiging Course" durations="2 months" />
+          <Header heading="Graphic Desiging Course" durations="2 months" />
         </ScrollView>
       </View>
     </View>
